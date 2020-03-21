f2ea31d4834917c9614f62bd67c637ed37c213a3
stefan521 <muscalu.stefan@yahoo.com>
2020-03-21 19:41:35 +0000

    Finished directives sections.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
-import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
+import {HighlightedDirective} from './directives/highlighted.directive';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +20,8 @@ export class AppComponent implements AfterViewInit {
 
     courses = COURSES;
 
+    @ViewChild(CourseCardComponent, {read: HighlightedDirective})
+    highlighted: HighlightedDirective;
 
     @ViewChildren(CourseCardComponent, {read: ElementRef})
     cards : QueryList<ElementRef>;
@@ -22,11 +32,15 @@ export class AppComponent implements AfterViewInit {
     }
 
     ngAfterViewInit() {
-
+      console.log(this.highlighted);
     }
 
     onCourseSelected(course:Course) {
 
+    }
+
+    onToggle(isHighlighted: boolean) {
+      console.log(isHighlighted);
     }
 
 }
---
+++ src/app/directives/highlighted.directive.ts
@@ -0,0 +1,47 @@
+import { 
+  Directive,
+  HostBinding,
+  HostListener,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
+
+@Directive({
+  selector: '[highlighted]',
+  exportAs: 'hl'
+})
+export class HighlightedDirective {
+
+  @Input('highlighted')
+  isHighlighted = false;
+
+  @Output()
+  toggleHighlight = new EventEmitter();
+
+  constructor() {
+  }
+
+  @HostBinding('class.highlighted')
+  get cssClasses() {
+    return this.isHighlighted;
+  }
+
+  @HostListener('mouseover')
+  mouseOver() {
+    this.isHighlighted = true;
+    this.toggleHighlight.emit(this.isHighlighted);
+  }
+
+  @HostListener('mouseleave')
+  mouseLeave() {
+    this.isHighlighted = false;
+    this.toggleHighlight.emit(this.isHighlighted);
+  }
+
+  toggle() {
+    this.isHighlighted = !this.isHighlighted;
+    this.toggleHighlight.emit(this.isHighlighted);
+  }
+
+}
